89680501129d6c093b63ebe990ec641b007ef44d
bently.sengrath <bently.sengrath@AFSWW5Q6FVQZEQ.us.accenturefederal.com>
2021-09-09 09:38:45 -0500

    Added MockGameDao
    Modified GameServiceImplementation and added MockDao file

--- mgl-webapp/src/main/java/com/organization/mvcproject/service/GameServiceImplementation.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/service/GameServiceImplementation.java
@@ -3,8 +3,10 @@ package com.organization.mvcproject.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.organization.mvcproject.dao.MockGameDao;
 import com.organization.mvcproject.model.Game;
 
 @Service
@@ -17,47 +19,21 @@ public class GameServiceImplementation implements GameRetrievalService {
 	 *  Move this list, and methods operating on this list to an appropriately named package and class.
 	 */
 	
-	private static Long gameId = new Long(0);
-	private static Long companyId = new Long(0);
-	private static List<Game> games = new ArrayList<Game>();
-
-	static {
-		games = populateGames();
-	}
-
-	private static List<Game> populateGames() {
-
-		Game game1 = new Game();
-		game1.setId(++gameId);
-		game1.setGenre("Sport");
-		game1.setName("Rocket League");
-
-		Game game2 = new Game();
-		game2.setId(++gameId);
-		game2.setGenre("Shooter");
-		game2.setName("Halo 3");
-
-		Game game3 = new Game();
-		game3.setId(++gameId);
-		game3.setGenre("MMORPG");
-		game3.setName("Runescape");
-
-		games.add(game1);
-		games.add(game2);
-		games.add(game3);
-
-		return games;
-	}
-
+	@Autowired
+	private MockGameDao mockgameDao;
+	//put all the methods that modify the data into this file from the mockDao.
 	@Override
 	public List<Game> retrieveAllGames() {
-		return games;
+		return mockgameDao.getAllGames();
 	}
 
 	@Override
 	public Game saveGame(Game game) {
-		game.setId(++gameId);
-		games.add(game);
-		return game;
+		return mockgameDao.saveGame(game);
+	}
+	
+
+	public Boolean deleteGame(long id){
+		return mockgameDao.deleteGame(id);
 	}
 }
